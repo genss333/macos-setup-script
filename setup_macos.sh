d2c72a3eb29d9b08dc478274de03d26bcf22e23f
Tatsapon Munsan <109135068+genss333@users.noreply.github.com>
2024-12-22 19:49:06 +0700

    Update setup_macos.sh

--- setup_macos.sh
+++ setup_macos.sh
@@ -5,30 +5,6 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-# Install or Update Homebrew
-if command_exists brew; then
-    echo "Checking Homebrew version..."
-    HOMEBREW_VERSION=$(brew --version | head -n 1 | awk '{print $2}')
-    REQUIRED_VERSION="3.0.0"
-
-    if [[ "$HOMEBREW_VERSION" < "$REQUIRED_VERSION" ]]; then
-        echo "Homebrew version $HOMEBREW_VERSION found but is outdated. Uninstalling..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
-        echo "Reinstalling Homebrew..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        echo 'eval "$($(brew --prefix)/bin/brew shellenv)"' >> ~/.zprofile
-    else
-        echo "Homebrew version $HOMEBREW_VERSION is up to date."
-    fi
-else
-    echo "Homebrew is not installed. Installing..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo 'eval "$($(brew --prefix)/bin/brew shellenv)"' >> ~/.zprofile
-fi
-
-# Reload terminal session to ensure changes take effect
-source ~/.zprofile
-
 # Verify Homebrew setup
 if ! command_exists brew; then
     echo "Homebrew setup failed. Uninstalling..."
